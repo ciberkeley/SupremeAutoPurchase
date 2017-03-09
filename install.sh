909d44b5bd6c1021c5d92146977b08d18c8fc129
Brandon Flannery <brandon.flannery@pluribuslabs.com>
2017-03-09 13:30:15 -0800

    Adding homebrew and chromedriver installation to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo brew install chromedriver
 sudo pip install -r requirements.txt
 ECHO " __________ 
 < Let's go!>
